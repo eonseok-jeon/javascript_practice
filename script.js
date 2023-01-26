ad7cb1795b3869b44fdad4afe7d6d4d6671cff99
Jeon Eonseok <jseokeon@gmail.com>
2023-01-27 01:12:00 +0900

    0127: esc keydown

--- script.js
+++ script.js
@@ -1,48 +1,25 @@
-'use strict';
+const showModalBtn = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
 
-let value = Math.trunc(Math.random() * 10) + 1;
-let score = 20;
-let highScore = 0;
-let setMessage = (message) => {
-  document.querySelector('.message').textContent = message;
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-document.querySelector('.check').addEventListener('click', () => {
-  const guess = Number(document.querySelector('.guess').value);
-  if (!guess) {
-    document.querySelector('.message').textContent = 'NaN';
-  } else if (value === guess) {
-    document.querySelector('.number').textContent = value;
-    setMessage('You Win');
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').style.backgroundColor = 'green';
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector('.highscore').textContent = score;
-    }
-  } else {
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      if (value > guess) {
-        setMessage('Too Low');
-      } else if (value < guess) {
-        setMessage('Too High');
-      }
-    } else {
-      setMessage('You Lose');
-      document.querySelector('.score').textContent = 0;
-    }
-  }
+document.querySelector('.close-modal').addEventListener('click', closeModal);
 
-  document.querySelector('.again').addEventListener('click', () => {
-    score = 20;
-    value = Math.trunc(Math.random() * 10) + 1;
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.number').textContent = '?';
-    setMessage('Start guessing...');
-    document.querySelector('.guess').value = '';
-    document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.number').style.backgroundColor = 'white';
-  });
+document.querySelector('.overlay').addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
